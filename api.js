78793aa95fbc59b81f4d260c9eb96f40ef7870b7
DenisRychlikowski <drychlikowski@gmail.com>
2023-04-18 17:58:09 +0200

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import { renderProducts } from "./dom-utils.js";
 
 const downloadata = async () => {
     try{
-        let data = await fetch("../produkty.json");
+        let data = await fetch("./produkty.json");
         let products = await data.json();
         renderProducts(products);
     }
